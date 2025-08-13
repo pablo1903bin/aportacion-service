8d88d3ed82d451c75ca276eff4c894a72e6a6e1f
pablo-linux <pablolidcorp@gmail.com>
2025-08-12 20:10:18 -0600

    add import+

--- src/main/java/com/tesoramobil/aportacion/AportacionServiceApplication.java
+++ src/main/java/com/tesoramobil/aportacion/AportacionServiceApplication.java
@@ -2,6 +2,7 @@ package com.tesoramobil.aportacion;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
