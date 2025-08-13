8dc91fe9d9cee54cee2dca42ff1bb8d48455ad49
pablo-linux <pablolidcorp@gmail.com>
2025-08-12 18:15:26 -0600

    commit inicial

---
+++ src/main/java/com/tesoramobil/aportacion/AportacionServiceApplication.java
@@ -0,0 +1,13 @@
+package com.tesoramobil.aportacion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AportacionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AportacionServiceApplication.class, args);
+	}
+
+}
