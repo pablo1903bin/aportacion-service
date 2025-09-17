233e782b307ee64a4ebe97332b310d7f27809498
pablo-linux <pablolidcorp@gmail.com>
2025-09-17 15:05:43 -0600

    add open api-config security

--- src/main/java/com/tesoramobil/aportacion/config/OpenApiServerConfig.java
+++ src/main/java/com/tesoramobil/aportacion/config/OpenApiServerConfig.java
@@ -3,23 +3,35 @@ package com.tesoramobil.aportacion.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.servers.Server;
-
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.Components;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-import java.util.List;
-
-@Configuration
+import org.springframework.context.annotation.Configuration;@Configuration
 public class OpenApiServerConfig {
 	
   @Value("${api-doc.server-base-path:/aportacion-service}")
   private String serverBasePath;
-	  
+
   @Bean
   public OpenAPI customOpenAPI() {
-
     return new OpenAPI()
-        .servers(List.of(new Server().url(serverBasePath)));
+        .info(new Info().title("Users Service API").version("v1"))
+
+        // 1) "Servers": le dice a Swagger que la base de las llamadas es el GATEWAY
+        .addServersItem(new Server().url(serverBasePath))
+
+        // 2) "SecurityRequirement": marca que las operaciones requieren "bearerAuth"
+        .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
+
+        // 3) "SecurityScheme": define qué es "bearerAuth" (Authorization: Bearer <JWT>)
+        .components(new Components().addSecuritySchemes(
+            "bearerAuth",
+            new SecurityScheme()
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("bearer")
+                .bearerFormat("JWT")));
   }
 }
